618671d85a871d756cad6f1eeda63c4ca73b627d
Caine Daniel <admin@cainethings.com>
2023-03-06 11:37:42 +0530

    Boiler

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./app.scss";
+const App = () => {
+  return (
+    <div>
+      <h1 className="heading">Nitros</h1>
+      <h4 className="sub-heading">by DineDroid</h4>
+    </div>
+  );
+};
+
+export default App;
